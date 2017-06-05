db6ed38cb2ea8237871c8540d16612b3ac57d497
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-06-05 20:25:49 +0000

    Quiz 2: Setting up a Database

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,22 @@
 class IdeasController < ApplicationController
 
-def index
-end
+  def index
+    @ideas = Idea.all
+  end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    Idea.create(idea_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :highlight)
+  end
 
 end
